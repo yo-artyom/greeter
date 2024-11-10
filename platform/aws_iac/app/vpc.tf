3bacdca4a26c048faa4eac15a5696ba1a202a6e3
Artem Malyshev <artem.malyshev@on-running.com>
2024-11-10 19:04:05 +0100

    add eks terraform

--- platform/aws_iac/app/vpc.tf
+++ platform/aws_iac/app/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "eks-base-vpc"
 
   cidr = "10.0.0.0/16"
-  azs  = ["eu-north-1"]
+  azs  = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
